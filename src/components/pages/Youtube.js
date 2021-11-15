24632112e1d976f38b589639756feab16a9cfb1c
seonyoung0403 <87642309+KIMSEOUNYOUNG@users.noreply.github.com>
2021-11-15 19:05:02 +0900

    21.11.15

--- src/components/pages/Youtube.js
+++ src/components/pages/Youtube.js
@@ -1,17 +1,67 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
 import Contents from '../layouts/Contents';
+import YoutubeList from '../info/YoutubeList';
+import YoutubeSearch from '../info/YoutubeSearch';
+import ContInfo from '../layouts/ContInfo';
+import WrapTitle from '../layouts/WrapTitle';
+function Youtube() {
+    const [videos, setVideos] = useState([]);
+    const start = () => {
+        setTimeout(() => {
+            alert("ddd");
+        }, 2000);
+    }
 
-function youtube(){
+    const search = (query) => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch(``, requestOptions)
+        .then(response => response.json())
+        .then(result => setVideos(result.items))
+        .catch(error => console.log('error', error));
+    }
+
+
+    useEffect(() => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=alsdk&key=AIzaSyDCYFDZID7nwDFsNWItUhGJ-JerhdZllVU&maxResults=30&type=video", requestOptions)
+        .then(response => response.json())
+        //.then(result => console.log(result.items))
+        .then(result => {
+            setVideos(result.items);
+            start();
+        })
+        .catch(error => console.log('error', error));
+    }, []);
     return (
         <div>
             <Header />
-            <Contents>youtube</Contents>
+            <Contents>
+                <section id="youtubeCont">
+                    <div className="container">
+                        <WrapTitle text={["youtube", "reference"]} />
+                        <div className="youtube__cont">
+                            <YoutubeSearch onSearch={search} />
+                            <div className="youtube">
+                                <YoutubeList videos={videos} />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <ContInfo />
+            </Contents>
             <Footer />
         </div>
-        
-        )
+    )
 }
-
-export default youtube;
+export default Youtube;
